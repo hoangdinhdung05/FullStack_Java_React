272780bac5666e56c11425093a60a354f52373fd
hoangdinhdung05 <hoangdinhdung0205@gmail.com>
2025-03-10 00:39:58 +0700

    first commit

---
+++ src/main/java/vn/hoangdung/restAPI/UserController.java
@@ -0,0 +1,15 @@
+package vn.hoangdung.restAPI;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class UserController {
+    
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
