e3c795831ef7b4c88cb0a4e78cb5c84e382bcdc4
hoangdinhdung05 <hoangdinhdung0205@gmail.com>
2025-03-29 11:30:46 +0700

    fix jwt

--- src/main/java/vn/hoangdung/restAPI/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/vn/hoangdung/restAPI/config/CustomAuthenticationEntryPoint.java
@@ -5,8 +5,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import vn.hoangdung.restAPI.domain.RestResponse;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
 import org.springframework.security.web.AuthenticationEntryPoint;
