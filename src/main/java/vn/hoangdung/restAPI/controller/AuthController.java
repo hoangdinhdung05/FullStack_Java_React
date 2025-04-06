72055590dd202bca8e9b0405c16e99cf5154e779
hoangdinhdung05 <hoangdinhdung0205@gmail.com>
2025-04-06 17:09:12 +0700

    fix API login

--- src/main/java/vn/hoangdung/restAPI/controller/AuthController.java
+++ src/main/java/vn/hoangdung/restAPI/controller/AuthController.java
@@ -8,12 +8,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import jakarta.validation.Valid;
-
 import org.springframework.web.bind.annotation.RequestBody;
+import vn.hoangdung.restAPI.domain.User;
 import vn.hoangdung.restAPI.domain.dto.LoginDTO;
 import vn.hoangdung.restAPI.domain.dto.ResLoginDTO;
+import vn.hoangdung.restAPI.service.UserService;
 import vn.hoangdung.restAPI.util.SecurityUtil;
 
 @RestController
@@ -22,10 +22,12 @@ public class AuthController {
 
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final SecurityUtil securityUtil;
+    private final UserService userService;
 
-    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil) {
+    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil, UserService userService) {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
         this.securityUtil = securityUtil;
+        this.userService = userService;
     }
     
     @PostMapping("/login")
@@ -38,8 +40,19 @@ public class AuthController {
 
         //Create token
         String access_token = this.securityUtil.createToken(authentication);
-        SecurityContextHolder.getContext().setAuthentication(authentication); 
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
         ResLoginDTO res = new ResLoginDTO();
+        User currentUserDB = this.userService.handleGetUserByUsername(loginDTO.getUsername());
+        if(currentUserDB != null) {
+            ResLoginDTO.UserLogin userLogin = new ResLoginDTO.UserLogin(
+                    currentUserDB.getId(),
+                    currentUserDB.getEmail(),
+                    currentUserDB.getName()
+            );
+            res.setUser(userLogin);
+        }
+        //set token
         res.setAccessToken(access_token);
         return ResponseEntity.ok().body(res);
 
--- src/main/java/vn/hoangdung/restAPI/domain/dto/LoginDTO.java
+++ src/main/java/vn/hoangdung/restAPI/domain/dto/LoginDTO.java
@@ -1,29 +1,15 @@
 package vn.hoangdung.restAPI.domain.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
-
+@Getter
+@Setter
 public class LoginDTO {
 
     @NotBlank(message = "Email không được để trống")
     private String username;
     @NotBlank(message = "Password không được để trống")
     private String password;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }    
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
 }
